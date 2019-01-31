65a698d35a7136bdf073fa71b53af701c49cc4b7
naohiro-t <takahashi.naohiro1222@gmail.com>
2019-01-31 18:11:44 +0900

    scaffold User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # use haml by default for generator
+  config.generators do |g|
+    g.template_engine :haml
+  end
 end
