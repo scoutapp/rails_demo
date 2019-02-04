681f9ac437b49bc3dee5d7d79efef3d17e1b2d9d
naohiro-t <takahashi.naohiro1222@gmail.com>
2019-02-04 17:39:17 +0900

    add pagination to users

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem 'httparty'
 # For bulk insert
 gem 'activerecord-import'
 
+# pagination
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do  
   root to: 'home#show'
 
-  resources :users
-  resources :beers, only: [:show, :index]
+  resources :users do
+    resources :drink_histories
+  end
+  resources :beers, only: [:show, :index] do
+    resources :drink_histories
+  end
+
   resources :drink_histories
 end
